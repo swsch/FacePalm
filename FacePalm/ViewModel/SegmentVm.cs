6cc9bb87201d5b4c1e3b6293f703a8acfc5a3322
Stefan Schmiedl <stefan@xss.de>
2018-03-05 00:04:58 +0100

    cosmetics

--- FacePalm/ViewModel/SegmentVm.cs
+++ FacePalm/ViewModel/SegmentVm.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using System.Xml.Schema;
 using FacePalm.Annotations;
 
 namespace FacePalm.ViewModel {
